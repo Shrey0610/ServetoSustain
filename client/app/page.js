2f1f95716880a26ce325e039796669f0e5a4140e
Shrey0610 <shreyshah241@gmail.com>
2024-04-14 21:11:19 +0530

    client

---
+++ client/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Button from 'react-bootstrap/Button';
+import {AccordionComp} from '../app/components/accordian.js';
+
+import {Navbarr} from '../app/components/navbar.js';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  <Navbarr />
+     
+    </main>
+  );
+}
